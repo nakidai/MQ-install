f8cfc12e4f4c438540517d106e06daa881e50889
Nakidai <plaza521@inbox.ru>
2024-03-07 20:57:59 +0300

    Check if version exists before downloading

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ string_contain() { case $2 in *$1* ) return 0;; *) return 1;; esac ;}
 
 DEFAULT_SL_PATH="/home/$(logname)/.local/share/Steam/steamapps/"
 DEFAULT_MQ_VER="1_1"
-VERSIONS="1 1_1"
 
 printf "Enter path to steamapps ($DEFAULT_SL_PATH): "
 read slpath
@@ -17,23 +16,31 @@ if ! [ -d "$slpath" ]; then
     exit 1
 fi
 
-printf "Enter MQ version (available: $VERSIONS) ($DEFAULT_MQ_VER): "
+printf "Enter MQ version ($DEFAULT_MQ_VER): "
 read mqver
 mqver=${mqver:-"$DEFAULT_MQ_VER"}
 
 bppath="MQr$mqver"
 
+if ! wget -O- "https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath" > /dev/null 2>&1; then
+    echo "There's no such version of MQ!"
+    exit 1
+fi
+
 echo "Downloading..."
+echo " blueprint.json..."
 mkdir -p "$slpath/workshop/387990/$bppath"
-if ! wget -O "$slpath/workshop/387990/$bppath/blueprint.json" "https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath/blueprint.json"; then
+if ! wget -O "$slpath/workshop/387990/$bppath/blueprint.json" "https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath/blueprint.json" > /dev/null 2>&1; then
     echo "Cannot download \"https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath/blueprint.json\", stopping..."
     exit 1
 fi
-if ! wget -O "$slpath/workshop/387990/$bppath/description.json" "https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath/description.json"; then
+echo " description.json..."
+if ! wget -O "$slpath/workshop/387990/$bppath/description.json" "https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath/description.json" > /dev/null 2>&1; then
     echo "Cannot download \"https://github.com/UltraQbik/SMC-MQ-CPU/raw/main/cpu_blueprints/$bppath/description.json\", stopping..."
     exit 1
 fi
-if ! wget -O "$slpath/workshop/387990/$bppath/icon.png" "https://github.com/UltraQbik/SMC-MQ-CPU/blob/main/cpu_blueprints/$bppath/icon.png?raw=true"; then
+echo " icon.png..."
+if ! wget -O "$slpath/workshop/387990/$bppath/icon.png" "https://github.com/UltraQbik/SMC-MQ-CPU/blob/main/cpu_blueprints/$bppath/icon.png?raw=true" > /dev/null 2>&1; then
     echo "Cannot download \"https://github.com/UltraQbik/SMC-MQ-CPU/blob/main/cpu_blueprints/$bppath/icon.png?raw=true\""
     exit 1
 fi
